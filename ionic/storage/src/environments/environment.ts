13f92ed63f8edea7dfe1a86fa0a518bbad3293eb
asdev29 <asdev29@gmail.com>
2023-07-08 16:31:41 +0200

    a

--- ionic/storage/src/environments/environment.ts
+++ ionic/storage/src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyBUkXkMUcKCISdQ4CA3csC1eXnDFy-d0Vs",
-    authDomain: "led-storage.firebaseapp.com",
-    projectId: "led-storage",
-    storageBucket: "led-storage.appspot.com",
-    messagingSenderId: "574556486482",
-    appId: "1:574556486482:web:925391639e530c8f28188b"
+    //Replace
   }
 };
 
